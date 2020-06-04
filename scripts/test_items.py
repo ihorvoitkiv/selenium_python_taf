7f4b0a48e7cc149e48967a85b1aa2d4be4f9e92b
Ihor Voitkiv <60190914+ihorvoitkiv@users.noreply.github.com>
2020-06-04 21:37:48 +0300

    Delete test_items.py

--- scripts/test_items.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-import time
-
-def test_add2basket_button(browser):
-
-    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207")
-    time.sleep(5)
-    btn_element = browser.find_element(By.CSS_SELECTOR,'button.btn-add-to-basket')
-    assert btn_element.is_displayed(), "add to basket button is not displayed"

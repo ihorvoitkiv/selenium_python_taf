974d241fdcf57660de6ddebc0316ea683d297f6f
ihor-voitkiv <i.voitkiv27@gmail.com>
2020-06-06 13:49:53 +0300

    addded basket_page locators

--- pages/locators.py
+++ pages/locators.py
@@ -26,4 +26,5 @@ class ProductPageLocators():
 class BasketPageLocators():
     EMPTY_BASKET = (By.CSS_SELECTOR, ".basket-items")
     EMPTY_BASKET_MESSAGE = (By.CSS_SELECTOR, "#content_inner p")
+#
 
